eaa38596d18909a933e62e1d8e9599c35fd1f180
holt83 <simoh@vejlebib.dk>
2018-06-14 17:02:30 +0200

    3594: Implement Webtreek cookie opt-out

---
+++ modules/ding_webtrekk/js/ding_webtrekk.opt-out.js
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * Implement opt-out option for the Webtrekk cookie.
+ */
+
+(function($) {
+  Drupal.behaviors.ding_webtrekk = {
+    attach: function(context, settings) {
+      // If Webtrekk is not setup correctly, we might not have this.
+      if (typeof webtrekkV3 === 'undefined') {
+        return;
+      }
+
+      $('.webtrekk-opt-out', context).click(function() {
+        webtrekk = new webtrekkV3;
+        // track the opt-out event in Webtrekk.
+        webtrekk.sendinfo({linkId: 'event_optout'});
+        // Set the opt-out cookie (for 5 years).
+        webtrekk.setCookie ('webtrekkOptOut', 1, 60 * 60 * 24 * 30 * 12 * 5);
+        alert(Drupal.t('Dear user, you have successfully excluded yourself from tracking.'));
+        return false;
+      })
+    }
+  }
+})(jQuery);
