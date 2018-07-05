bd0323e20d79c315273000e6f9765084f7452c2c
holt83 <simoh@vejlebib.dk>
2018-07-05 15:38:13 +0200

    3594: Scrutinizer fixes

--- modules/ding_webtrekk/js/ding_webtrekk.opt-out.js
+++ modules/ding_webtrekk/js/ding_webtrekk.opt-out.js
@@ -4,22 +4,25 @@
  */
 
 (function($) {
+
+  "use strict";
+
   Drupal.behaviors.ding_webtrekk = {
-    attach: function(context, settings) {
+    attach: function(context) {
       // If Webtrekk is not setup correctly, we might not have this.
       if (typeof webtrekkV3 === 'undefined') {
         return;
       }
 
       $('.webtrekk-opt-out', context).click(function() {
-        webtrekk = new webtrekkV3;
+        webtrekk = new webtrekkV3();
         // track the opt-out event in Webtrekk.
         webtrekk.sendinfo({linkId: 'event_optout'});
         // Set the opt-out cookie (for 5 years).
         webtrekk.setCookie ('webtrekkOptOut', 1, 60 * 60 * 24 * 30 * 12 * 5);
         alert(Drupal.t('Dear user, you have successfully excluded yourself from tracking.'));
         return false;
-      })
+      });
     }
-  }
+  };
 })(jQuery);
